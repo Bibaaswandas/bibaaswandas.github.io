6754922f399d39ce0210cc3f7d5135813e1d7861
Bibaaswandas <71015395+Bibaaswandas@users.noreply.github.com>
2021-03-13 19:05:37 +0530

    added few animations
    few animations, some styling on the form

--- js/app.js
+++ js/app.js
@@ -38,3 +38,44 @@ $('.sliderart').slick({
       }
     ]
   });
+
+
+  var targetabout = document.querySelector("#buttonnice");
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY >= targetabout.getBoundingClientRect().top) {
+      targetabout.classList.add('animate__animated')
+      targetabout.classList.add('animate__fadeIn')
+    }
+  });
+
+  var targetserve1 = document.querySelector(".moveleft");
+  var targetserve2 = document.querySelector(".moveleft2");
+  var targetserve3 = document.querySelector(".moveleft3");
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY >= targetserve1.getBoundingClientRect().top) {
+      targetserve1.classList.add('animate__animated')
+      targetserve1.classList.add('animate__slideInRight')
+      targetserve2.classList.add('animate__animated')
+      targetserve2.classList.add('animate__slideInRight')
+      targetserve3.classList.add('animate__animated')
+      targetserve3.classList.add('animate__slideInRight')
+    }
+  });
+
+  var targetserve4 = document.querySelector(".moveright");
+  var targetserve5 = document.querySelector(".moveright2");
+  var targetserve6 = document.querySelector(".moveright3");
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY >= targetserve4.getBoundingClientRect().top) {
+      targetserve4.classList.add('animate__animated')
+      targetserve4.classList.add('animate__slideInLeft')
+      targetserve5.classList.add('animate__animated')
+      targetserve5.classList.add('animate__slideInLeft')
+      targetserve6.classList.add('animate__animated')
+      targetserve6.classList.add('animate__slideInLeft')
+    }
+  });
+
